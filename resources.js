2f04d39c6cfd022c8c90eafdba89766194834495
Gordana Minovska <ekogoca@gmail.com>
2017-04-13 13:53:00 -0700

    Display "Not Found" message

--- resources.js
+++ resources.js
@@ -17,7 +17,11 @@ var myResources = [
 ];
 
 function getResource(name, resources = myResources) {
-    return resources.find((item) => item.name.toLowerCase() == name.toLowerCase());    
+    var result = resources.find((item) => item.name.toLowerCase() == name.toLowerCase()); 
+    if(!result) {
+        return "Not found";
+    }
+    return result;  
 }
 
 module.exports = {
