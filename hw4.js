dd30c39124e90f6d4530dbd3ab8659ab0806c8d8
Gordana Minovska <ekogoca@gmail.com>
2017-04-27 11:38:58 -0700

    Fix ESlint errors

--- hw4.js
+++ hw4.js
@@ -1,7 +1,8 @@
 // eslint practice file for ITC 230
-names = ['sara','joe','dave','ann']
-var newArray = names.map( function(item) {
- return item.toUpperCase();
- x = 2;
-}) 
-console.log(newArray);
+var names = ["sara","joe","dave","ann"];
+var newArray = names.map( function(item) {  
+	return item.toUpperCase();   
+}); 
+
+//assign the values from newArray to the names (solves unused variable eslint error)
+names = newArray;
