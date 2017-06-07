0922e03c2ed76dc33a6a07adc830bee21c9dda34
Gordana Minovska <ekogoca@gmail.com>
2017-06-07 12:45:22 -0700

    Refactor index.js
    The ajax call that gets the data is now made from the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 
+//make an ajax call to get the data
+import axios from 'axios';
+
 //import components
 import Jumbotron from './Jumbotron';
 import ResourceList from './ResourceList';
 
 class App extends Component {
+
+    constructor(props){
+    super(props);
+     this.state = { 
+         resources: []
+         , filterText: ''
+         , selectedResource: null
+     };   
+    }
+    
+    componentWillMount() {
+        axios
+        .get('/api/resources')
+        .then((response) => { this.setState({resources: response.data});})
+        .catch(e => {console.log(e);})
+    }
+    
     
     render() {
         return (
             //pass the data from the App props to the Resource List Component
             <div className="container">
-            <Jumbotron />
-            <ResourceList resources={this.props.resources}/> 
+            <Jumbotron onInputChange={(text)=>{this.setState({filterText: text.toLowerCase()});}}/>
             </div> 
         );
     }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,8 @@ import ReactDOM from 'react-dom';
 //import components
 import App from './components/App.js';
 
-//make an ajax call to get the data
-import axios from 'axios';
-axios
-    .get('/api/resources')
-    .then((response) => {
         ReactDOM.render(
-            //pass the data returned from the api call to the app component
-        <App resources={response.data}/>, 
+        <App />, 
         document.getElementById("root"));
-    })
-    .catch(err => console.log(err))
     
     
